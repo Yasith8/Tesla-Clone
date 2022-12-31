6ec0c106a3054433abf7eadcd68977e1eb2097c8
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-31 15:52:37 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,15 @@
+function ms() {
+    document.getElementById("header").style.backgroundImage = "url(imaage-1.png)";
+}
+
+function m3() {
+    document.getElementById("header").style.backgroundImage = "url(imaage-2.png)";
+}
+
+function mx() {
+    document.getElementById("header").style.backgroundImage = "url(imaage-3.png)";
+}
+
+function my() {
+    document.getElementById("header").style.backgroundImage = "url(imaage-4.png)";
+}
